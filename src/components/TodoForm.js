845fc38927fb5b9e0f929d691c930c756d1087f6
dwayneglevene <dwayneglevene@yahoo.com>
2021-01-29 02:59:39 +0000

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import uuid from "uuid";
+
+
+function TodoForm({addTodo}){
+    const [todo,setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    function handleTaskInputChange(e){
+        setTodo({...todo, task:e.target.value});
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+//  ONLY GETS CALLED IF TO DO TASK IS NOT EMPPY REMOVES WHITE SPACE FROM STRING
+        if(todo.task.trim()){
+            addTodo({...todo, id: uuid.v4()});
+
+        }
+    }
+    return(
+        <form>
+            <input name="task" type="text" value={todo.task}  onchange={handleTaskInputChange}/>
+            <buttom />
+        </form>
+
+    );
+}
+
+
+
+
+
+export default TodoForm;
